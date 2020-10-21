fa01d4ac81b296c0abca23b4ff5c3d3052ed6462
Md.Noushedul Islam <noushedakib@gmail.com>
2020-10-21 11:38:58 +0600

    js for,while,do while & nested loop

---
+++ js/loop.js
@@ -0,0 +1,55 @@
+// for loop 
+for(k=1;k<10;k++){
+	console.log("Noushedul isalm");
+	// document.write("akib<br>");
+}
+
+var j = "noushedul isalm";
+for(i=0;i<j.length;i++){
+	console.log("akib");
+	// document.write("akib<br>");
+}
+
+var q = 10;
+for(q=0;q<10;q++){
+	console.log(q);
+	// document.write(q);
+}
+
+// While loop
+var r=0;
+while(r<15){
+	console.log(r);
+	// document.write(r);
+	r++;
+}
+
+var our = 0;
+while(true){
+	if(our==4){
+		break;
+	}
+	console.log(our);
+	our++;
+}
+
+var cat = 0;
+while(cat<3){
+	console.log("cat");
+	document.write("cat<br>");
+	cat++;
+	mouse = 0;
+	while(mouse<5){
+		console.log("mouse");
+		document.write("mouse<br>");
+		mouse++;
+	}
+}
+
+// do while loop
+var country = 0;
+do{
+	document.write("Southern<br>");
+	console.log("Southern");
+	country++;
+}while(country<5);
